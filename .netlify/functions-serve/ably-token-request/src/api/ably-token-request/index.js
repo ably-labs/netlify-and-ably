382a7b9b977880c1fd49965c9370019cfffee5ed
Jo Franchetti <jofranchetti@gmail.com>
2022-08-15 17:52:52 +0100

    updating readme and index

--- .netlify/functions-serve/ably-token-request/src/api/ably-token-request/index.js
+++ .netlify/functions-serve/ably-token-request/src/api/ably-token-request/index.js
@@ -18249,8 +18249,8 @@ async function handler(event, context) {
       statusCode: 500,
       headers: { "content-type": "application/json" },
       body: JSON.stringify(`Missing ABLY_API_KEY environment variable.
-        If you're running locally, please ensure you have a ./api/.env file with a value for ABLY_API_KEY=your-key.
-        If you're running in Azure, make sure you've configured the AppSettings ABLY_API_KEY. 
+        If you're running locally, please ensure you have a ./.env file with a value for ABLY_API_KEY=your-key.
+        If you're running in Netlify, make sure you've configured env variable ABLY_API_KEY. 
         Please see README.md for more details on configuring your Ably API Key.`)
     };
   }
